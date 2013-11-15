7f10b11317524cf065bed6a8b07f2283fea770d2
915546302 <915546302@163.com>
2013-11-15 23:21:55 +0800

    modified:   tinyproxy.py 	support https,but need improve...

--- tinyproxy.py
+++ tinyproxy.py
@@ -13,9 +13,9 @@ class Proxy:
 		self.method=None
 		self.targetHost=None
 	def getClientRequest(self):
-		request=self.client.recv(self.BUFSIZE)
+		request=self.client.recv(self.BUFSIZE)	
 		if not request:
-			return None
+			return 
 		cn=request.find('\n')
 		firstLine=request[:cn]
 		print firstLine[:len(firstLine)-9]
@@ -27,18 +27,14 @@ class Proxy:
 		tmp=self.targetHost.split('/')
 		net=tmp[0]+'//'+tmp[2]
 		request=request.replace(net,'')
-		targetAddr=self.getTargetInfo(tmp[2])
-		try:
-			(fam,_,_,_,addr)=socket.getaddrinfo(targetAddr[0],targetAddr[1])[0]
-		except Exception as e:
-			print e
-			return
-		self.target=socket.socket(fam)
-		self.target.connect(addr)
+		self.getTargetInfo(tmp[2])
 		self.target.send(request)
 		self.nonblocking()
 	def connectMethod(self,request):
-		pass
+		con='HTTP/1.1 200 Connection established\r\nProxy-agent: tinyproxy0.1\r\n\r\n'
+		self.client.send(con)
+		self.getTargetInfo(self.targetHost)
+		self.nonblocking()
 	def run(self):
 		request=self.getClientRequest()
 		if request:
@@ -46,10 +42,10 @@ class Proxy:
 				self.commonMethod(request)
 			elif self.method=='CONNECT':
 				self.connectMethod(request)
-	def nonblocking(self):
+	def nonblocking(self,timeout=3):
 		inputs=[self.client,self.target]
 		while True:
-			readable,writeable,errs=select.select(inputs,[],inputs,3)
+			readable,_,errs=select.select(inputs,[],inputs,timeout)
 			if errs:
 				break
 			for soc in readable:
@@ -73,19 +69,31 @@ class Proxy:
 		else:
 			site=host
 			port=80
-		return site,port
+		try:
+			(fam,_,_,_,addr)=socket.getaddrinfo(site,port)[0]
+		except Exception as e:
+			print e
+			return
+		self.target=socket.socket(fam)
+		self.target.connect(addr)
 
-if __name__=='__main__':	  
+if __name__=='__main__':
+	from multiprocessing import Process
 	host = '127.0.0.1' 
 	port = 8083
-	backlog = 5 
-	server = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
-	server.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+	#server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR , 1)
 	server.bind((host,port)) 
 	server.listen(5) 
-	while True:
-		thread.start_new_thread(Proxy(server).run,())
-		# p=Process(target=Proxy(server).run, args=()) #多进程
-		# p.start()
+	try:
+		while True:
+			thread.start_new_thread(Proxy(server).run,())
+			# 	p=Process(target=Proxy(server).run,args=())
+			# 	p.start()
+			#	p.join()
+	except KeyboardInterrupt:
+		server.close()
+		sys.exit(0)
+	
 		
    
